ad17f8fb834195f7dd467b44c02bdbc7f2665f98
Joe Prewitt <justjoe22@gmail.com>
2016-09-16 19:55:56 +0000

    Flex Started

--- server.js
+++ server.js
@@ -19,13 +19,35 @@ storageBucket: "workflow-flex.appspot.com",
 firebase.initializeApp(config);
 
 firebase.auth().onAuthStateChanged(function(user) {
+    var vHtml = '';
+      
         if (user) {
             console.log('Success: ' + user.uid);
             
+              vHtml += '<br /><p>You are logged in as <b>' + response.user_name + '</b>...</p><br />';
+              vHtml += '<br /><p><b>My Form</b></p><br />';
+              vHtml += controls.controldef('input','text','Sample','myID','');
+
         }
         else {
             console.log('Login Failed.');
+            
+              vHtml += '<form action="/login" method="POST">';
+              vHtml += 'User: <input type="email" name="User_Name"><br>';
+              vHtml += 'Password: <input type="password" name="password">';
+              vHtml += '<input type="submit" value="Submit">';
+              vHtml += '</form>';
+              
         }
+        
+        /* global $ */
+        $("./masterPage").html(vHtml);
+        
+});
+
+app.all('/secret', function (req, res, next) {
+  console.log('Accessing the secret section ...');
+  next(); // pass control to the next handler
 });
 
 var bodyParser = require('body-parser');
@@ -37,11 +59,11 @@ app.use(express.static('public'));
 //app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(multer({ dest: 'tmp'}));
 
-app.get('/index.html', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
+app.get('/main.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "main.html" );
 })
 
-app.post('/login_post', urlencodedParser, function (req, res) {
+app.post('/login', urlencodedParser, function (req, res) {
     
    // Prepare output in JSON format
    /*global response*/
@@ -65,40 +87,29 @@ app.post('/login_post', urlencodedParser, function (req, res) {
       // [END_EXCLUDE]
     });
     // [END authwithemail]
-   
-   //console.log(response);
-   var vHtml = '<br /><p>You are logged in as <b>' + response.user_name + '</b>...</p><br />';
-   vHtml += '<br /><p><b>My Form</b></p><br />';
-   vHtml += controls.controldef('input','text','Sample','myID','');
-   
-   res.end(vHtml);
+
+    res.end();
    
 })
 
-// This responds with "Hello World" on the homepage
-app.get('/', function (req, res) {
-   console.log("Got a GET request for the homepage");
-   res.send('Hello GET');
-})
 
+// // This responds a POST request for the homepage
+// app.post('/', function (req, res) {
+//   console.log("Got a POST request for the homepage");
+//   res.send('Hello POST');
+// })
 
-// This responds a POST request for the homepage
-app.post('/', function (req, res) {
-   console.log("Got a POST request for the homepage");
-   res.send('Hello POST');
-})
+// // This responds a DELETE request for the /del_user page.
+// app.delete('/del_user', function (req, res) {
+//   console.log("Got a DELETE request for /del_user");
+//   res.send('Hello DELETE');
+// })
 
-// This responds a DELETE request for the /del_user page.
-app.delete('/del_user', function (req, res) {
-   console.log("Got a DELETE request for /del_user");
-   res.send('Hello DELETE');
-})
-
-// This responds a GET request for the /list_user page.
-app.get('/list_user', function (req, res) {
-   console.log("Got a GET request for /list_user");
-   res.send('Page Listing');
-})
+// // This responds a GET request for the /list_user page.
+// app.get('/list_user', function (req, res) {
+//   console.log("Got a GET request for /list_user");
+//   res.send('Page Listing');
+// })
 
 var server = app.listen(8081, function () {
 
